9e2dd325a97e0a16cba0fb2b51a42dde9a9c6d32
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-10-03 17:03:02 +0200

    chore: create a minified dist

--- packages/dataset/tsup.config.ts
+++ packages/dataset/tsup.config.ts
@@ -1,20 +1,29 @@
-import { defineConfig } from 'tsup'
+import { Options, defineConfig } from 'tsup'
+
+const config: Options = {
+  entry: {
+    index: './src/index.ts',
+  },
+  outDir: './dist',
+  format: ['esm'],
+  target: 'es2020',
+  ignoreWatch: ['**/dist/**', '**/node_modules/**', '*.test.ts'],
+  clean: true,
+  dts: true,
+  sourcemap: true,
+  splitting: true,
+  minify: false,
+  skipNodeModulesBundle: true,
+  external: ['node_modules'],
+}
 
 export default defineConfig([
+  config,
   {
+    ...config,
     entry: {
-      index: './src/index.ts',
+      'index.min': './src/index.ts',
     },
-    outDir: './dist',
-    format: ['esm'],
-    target: 'es2020',
-    ignoreWatch: ['**/dist/**', '**/node_modules/**', '*.test.ts'],
-    clean: true,
-    dts: true,
-    sourcemap: true,
-    splitting: true,
-    minify: false,
-    skipNodeModulesBundle: true,
-    external: ['node_modules'],
+    minify: true,
   },
 ])
