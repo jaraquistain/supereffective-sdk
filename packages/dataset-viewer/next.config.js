47a0024d900252f66224c58864690127c70c7776
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-10-10 00:55:40 +0200

    fix: build issues

--- packages/dataset-viewer/next.config.js
+++ packages/dataset-viewer/next.config.js
@@ -1,8 +1,10 @@
-const isDev = process.env.NODE_ENV === 'development'
+const isDev = process.env.VERCEL !== '1'
 
 const APP_DATA_URL = isDev ? 'http://localhost:4455/dist/data' : 'https://itsjavi.com/supereffective-sdk/data'
 const APP_ASSETS_URL = isDev ? 'http://localhost:4455/assets' : 'https://itsjavi.com/supereffective-assets/assets'
 
+console.log('VERCEL is: ', process.env.VERCEL)
+console.log('VERCEL_ENV is: ', process.env.VERCEL_ENV)
 console.log('NODE_ENV is: ', process.env.NODE_ENV)
 console.log('APP_DATA_URL is: ', APP_DATA_URL)
 console.log('APP_ASSETS_URL is: ', APP_ASSETS_URL)
